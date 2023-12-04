b0ee9f7f95b51ce1f062e8aa46aa58dae0505286
Ko-Win-Lwin <winlwin.mce@gmail.com>
2023-12-04 14:32:03 +0630

    Step-1: Desktop validation

--- src/components/Desktop/Detail/DetailView.vue
+++ src/components/Desktop/Detail/DetailView.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
     <YourInfo></YourInfo>
-    <div class="mt-16 flex justify-between mr-20">
-      <BackBtn></BackBtn>
-      <div class="ml-auto">
-        <NextBtn></NextBtn>
-      </div>
-    </div>
+
   </div>
 </template>
 
 <script setup>
 import YourInfo from './YourInfo.vue';
-import NextBtn from '../../NextBtn.vue'
-import BackBtn from '../../BackBtn.vue';
+
 
 
 </script>
--- src/components/NextBtn.vue
+++ src/components/NextBtn.vue
@@ -1,12 +1,14 @@
 <template>
   <div :class="{'hidden' : currentStep.$state.step == 4}">
-    <button @click="nextStep" class="px-5 py-2 rounded-md text-white font-semibold bg-sky-900">Next Step</button>
+    <button @click="$emit('infoSubmit')" class="px-5 py-2 rounded-md text-white font-semibold bg-sky-900">Next Step</button>
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import { useStepStore } from '../stores/step';
+
+defineEmits(['infoSubmit'])
 
 const currentStep = useStepStore();
 
